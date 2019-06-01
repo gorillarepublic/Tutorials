a2f6ddcfb032302411de818a7982c98eb96b64bd
andrewkledzik <andrew.kledzik@gmail.com>
2019-06-01 12:40:12 +0100

    Initial commmit

---
+++ Tutorial2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+#This is a blank window
+root = Tk()
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="Button 1", bg="black", fg="red")
+button2 = Button(topFrame, text="Button 2", fg="purple")
+button3 = Button(topFrame, text="Button 3", fg="yellow")
+button4 = Button(bottomFrame, text="Button 4", fg="orange")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=BOTTOM)
+
+#Display the window
+root.mainloop()
